3e9927350da6c65528de7378bf1e9b98dd30ce4f
MatthewCodes <matthewdatarhodes@gmail.com>
2019-11-12 11:14:38 -0800

    Added csv that will contain sample data after features are decided

--- match_id_gen.py
+++ match_id_gen.py
@@ -2,12 +2,20 @@ import requests
 import json
 import sys
 import time
+import csv
+
 URL = 'https://na1.api.riotgames.com/lol/match/v4/matches/3187138923'
 
 params = {'api_key':sys.argv[1]}
 
 r = requests.get(url = URL, params = params)
 player_ids = []
+with open('league.csv', 'wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',')
+
+features = []
+row = dict()
+#print(json.dumps(r.json(), indent=4))
 for player_id in r.json()["participantIdentities"]:
     print(player_id["player"]["accountId"])
     player_ids.append(player_id["player"]["accountId"])
@@ -17,15 +25,18 @@ for player in player_ids:
     URL = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'+ player
     print(URL)
     r = requests.get(url = URL, params = params)
-    print(json.dumps(r.json()))
+    #print(json.dumps(r.json()))
     #matches = [x for x in r.json()["matches"]]
     for match in r.json()["matches"]:
         gameIds.append(match["gameId"])
     print(len(gameIds))
-    print(gameIds)
-    time.sleep(10)
-    break
     
+    time.sleep(1)
+    
+print("All match Ids for every player in first match 3187138923")
+print(gameIds)
+
+# Note that one match ID can give us 1000 different matches
 for game in gameIds:
     gameURL = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + str(game)
     new_request = requests.get(url = gameURL, params = params)
